5c11bcfeb6cf37fb070bc8f114757873157fafbd
lukagabadze <77752229+lukagabadze@users.noreply.github.com>
2022-09-09 23:51:02 +0400

    Preset color themes (#2)
    * preset themes
    
    * removed urg colors from blue
    
    * increased border px
    
    * blue theme border color change

--- config.h
+++ config.h
@@ -1,25 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static int swallowfloating          = 0;   static const int topbar             = 1;        /* 0 means bottom bar */ static const char *fonts[]          = { "monospace:size=8" };
 static const char dmenufont[]       = "monospace:size=8";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#35bf5c";
-static const char col_red[]         = "#e81e39";
-//static const char *colors[][3]      = {
-	//[>               fg         bg         border   <]
-	//[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	//[SchemeSel]  = { col_gray4, col_cyan,  col_cyan },
-//};
 
-#include "/home/gabo/.cache/wal/colors-wal-dwm.h"
+
+/* color set */
+#include "./colors.h"
 
 
 /* tagging */
---
+++ themes/blue.h
@@ -0,0 +1,13 @@
+static const char norm_fg[] = "#bfd0e8";
+static const char norm_bg[] = "#000000";
+static const char norm_border[] = "#8591a2";
+
+static const char sel_fg[] = "#bfd0e8";
+static const char sel_bg[] = "#028bd5";
+static const char sel_border[] = "#1f9add";
+
+static const char *colors[][3]      = {
+    /*               fg           bg         border                         */
+    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+};
