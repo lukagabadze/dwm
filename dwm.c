3d4fea5d5dc5d508c8c20948e9d3fd91f7ffe959
lukagabadze <gabadzeluka2003@gmail.com>
2022-10-22 03:05:01 +0400

    toggle actual fullscreen

--- dwm.c
+++ dwm.c
@@ -227,6 +227,7 @@ static void tagnthmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void togglescratch(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
@@ -1878,6 +1879,13 @@ togglescratch(const Arg *arg)
 		}
 	} else
 		spawn(arg);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
